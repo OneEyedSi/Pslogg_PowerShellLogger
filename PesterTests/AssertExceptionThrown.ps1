1b56d191934991a57c55d21ae4a332f8ab36321f
SimonE <SimonE@datacom.co.nz>
2018-02-19 02:32:33 +1300

    Add tests of Write-LogMessage

---
+++ PesterTests/AssertExceptionThrown.ps1
@@ -0,0 +1,296 @@
+﻿<#
+.SYNOPSIS
+Function wrapper that tests the exceptions a function should generate.
+
+.DESCRIPTION
+Function wrapper that tests the exceptions a function should generate.  "Should -Throw" will 
+only check the exception message, not the type of exception.  This function can test both 
+exception message and type.
+
+.NOTES
+Usage is similar to that for Pester "Should -Throw": Wrap the function under test, along with 
+any arguments, in curly braces and pipe it to Assert-ExceptionThrown.
+
+.PARAMETER FunctionScriptBlock
+The function under test, including any arguments, wrapped in curly braces to form a scriptblock. 
+
+.PARAMETER ExpectedExceptionTypeName
+The type name of the exception that is expected to be thrown by the function under test.  
+
+By default this is the short name of the exception class, without a namespace.  If the 
+-UseFullTypeName switch is set then the full name of the exception class, including namespace, 
+must be specified.  The comparison between the expected and actual exception class names is 
+case insensitive.
+
+.PARAMETER ExpectedExceptionMessage
+All or part of the exception message that is expected when the function under test is run. 
+ 
+The test is effectively "Actual exception message must contain ExpectedExceptionMessage".  The 
+comparison between the expected and actual exception messages is case insensitive.
+
+.PARAMETER UseFullTypeName
+A switch parameter that affects the behaviour of the ExpectedExceptionTypeName comparison.  
+
+If UseFullTypeName is not set then the short name of the actual exception class, excluding any 
+namespace, is compared against ExpectedExceptionTypeName.  If UseFullTypeName is set then the 
+full name of the actual exception class, including namespace, is compared against 
+ExpectedExceptionTypeName.
+
+If ExpectedExceptionTypeName is not specified or is $Null or is an empty or blank string then 
+UseFullTypeName is ignored.
+
+.PARAMETER Not
+A switch parameter that inverts the test when set.  
+
+The effect of the Not parameter depends on whether ExpectedExceptionTypeName or 
+ExpectedExceptionMessage are set.  If neither ExpectedExceptionTypeName nor 
+ExpectedExceptionMessage are set then Not means "Function should not throw any exception."  
+
+If either ExpectedExceptionTypeName and/or ExpectedExceptionMessage are set then Not means 
+"Function should not throw an exception with the specified class name and/or message."  This 
+means the test will pass if no exception is thrown, or if an exception is thrown which does not 
+have the specified class name and/or message.
+
+.EXAMPLE 
+Test that a function taking two arguments throws an exception with a specified message
+
+{ MyFunctionWithTwoArgs -Key MyKey -Value 10 } | 
+    Assert-ExceptionThrown -ExpectedExceptionMessage 'Value was of type int32, expected string'
+
+The name of the function under test is MyFunctionWithTwoArgs.  The test will only pass if 
+MyFunctionWithTwoArgs, with the specified arguments, throws an exception with a message 
+that contains the specified text.
+
+.EXAMPLE 
+Test that a function taking no arguments throws an exception of a specified type
+
+{ MyFunction } | 
+    Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException
+
+The test will only pass if MyFunction throws an ArgumentException.
+
+.EXAMPLE 
+Test that a function throws an exception with the type name specified in full
+
+{ MyFunction } | 
+    Assert-ExceptionThrown -ExpectedExceptionTypeName System.ArgumentException `
+                            -UseFullTypeName
+
+The test will only pass if MyFunction throws a System.ArgumentException.
+
+.EXAMPLE 
+Test that a function does not throw an exception
+
+{ MyFunctionWithTwoArgs -Key MyKey -Value MyValue } | 
+    Assert-ExceptionThrown -Not
+
+The test will pass only if MyFunctionWithTwoArgs, with the specified arguments, does not throw 
+any exception.
+
+.EXAMPLE 
+Test that a function does not throw an exception with a specified message
+
+{ MyFunctionWithTwoArgs -Key MyKey -Value 10 } | 
+    Assert-ExceptionThrown -Not -ExpectedExceptionMessage 'Value was of type int32, expected string'
+
+The test will fail if MyFunctionWithTwoArgs, with the specified arguments, throws an exception 
+with a message that contains the specified text.  It will pass if MyFunctionWithTwoArgs does not 
+throw an exception, or if it throws an exception with a different message.
+
+.EXAMPLE 
+Test that a function does not throw an exception of a specified type
+
+{ MyFunction } | 
+    Assert-ExceptionThrown -Not -ExpectedExceptionTypeName ArgumentException
+
+The test will fail if MyFunction throws an ArgumentException.  It will pass if MyFunction does 
+not throw an exception, or if it throws an exception of a different type.
+#>
+function Assert-ExceptionThrown 
+(
+    [Parameter(
+        Position=0, 
+        ValueFromPipeline=$true)
+    ]
+    [scriptblock]$FunctionScriptBlock, 
+
+    [string]$ExpectedExceptionTypeName,
+    [string]$ExpectedExceptionMessage, 
+    [switch]$UseFullTypeName, 
+
+    [switch]$Not
+)
+{
+    process
+    {
+        if ($FunctionScriptBlock -eq $Null)
+        {
+            throw [ArgumentNullException] `
+                "Script block for function under test not found. Input to 'Assert-ExceptionThrown' must be enclosed in curly braces."
+        }
+
+        try
+        {
+            Invoke-Command -ScriptBlock $FunctionScriptBlock
+        }
+        catch
+        {
+            $errorMessages = Get-ExceptionError -Exception $_.Exception `
+                -ExpectedExceptionTypeName $ExpectedExceptionTypeName `
+                -ExpectedExceptionMessage $ExpectedExceptionMessage `
+                -UseFullTypeName:$UseFullTypeName -Not:$Not
+                
+            if ($errorMessages.Count -gt 0)
+            {
+                throw [System.Exception] ($errorMessages -join [Environment]::NewLine)
+            }            
+
+            return
+        }
+
+        # Will only get here if no exception was thrown by the function under test...
+        
+        if ([string]::IsNullOrWhiteSpace($ExpectedExceptionTypeName) -and 
+            [string]::IsNullOrWhiteSpace($ExpectedExceptionMessage))
+        {
+            # No expectations were specified for the exception type or the exception message.  
+            # So in this case -Not means "Should not throw any exception."
+            if ($Not)
+            {
+                return
+            }
+            
+            # No exception expectations were specified and -Not was also not specified.  This 
+            # means "Should throw an exception".
+            throw [System.Exception] "Expected an exception to be thrown but none was."
+        }
+
+        # If any exception expectation was specified then -Not means "Should not throw an 
+        # exception of the specified type and/or with the specified message, as appropriate."
+        # So not throwing an exception is a pass. 
+        if ($Not)
+        {
+            return
+        }
+
+        $errorMessages = @()
+
+        if (-not [string]::IsNullOrWhiteSpace($ExpectedExceptionTypeName))
+        {
+            $errorMessages += 
+                "Expected $ExpectedExceptionTypeName to be thrown but it wasn't."
+        }
+
+        if (-not [string]::IsNullOrWhiteSpace($ExpectedExceptionMessage))
+        {
+            $errorMessages += 
+                "Expected exception with message '$ExpectedExceptionMessage' to be thrown but it wasn't."
+        }
+
+        if ($errorMessages.Count -gt 0)
+        {
+            throw [System.Exception] ($errorMessages -join [Environment]::NewLine)
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+Gets an array of error messages where an exception does not meet expectations.
+
+.DESCRIPTION
+Gets an array of error messages where an exception does not meet expectations.  If all 
+expectations are met the array will be empty.
+#>
+function Get-ExceptionError
+(
+    [Exception]$Exception,
+
+    [string]$ExpectedExceptionTypeName,
+    [string]$ExpectedExceptionMessage, 
+    [switch]$UseFullTypeName, 
+
+    [switch]$Not
+)
+{
+    if ([string]::IsNullOrWhiteSpace($ExpectedExceptionTypeName) -and 
+        [string]::IsNullOrWhiteSpace($ExpectedExceptionMessage))
+    {
+        # No expectations were specified for the exception type or the exception message.  
+        # So in this case -Not means "Should not throw any exception."
+        if ($Not)
+        {
+            # Note leading comma to turn this error message into an array with a single item.
+            return ,"Expected no exception but $($Exception.GetType().FullName) was thrown with message '$($Exception.Message)'."
+        }
+            
+        # No exception expectations were specified and -Not was also not specified.  This 
+        # means "Should throw an exception, any exception".
+        return @()
+    }
+
+    $errorMessages = @()
+    $exceptionTypeMatched = $True
+    $exceptionMessageMatched = $True
+
+    if (-not [string]::IsNullOrWhiteSpace($ExpectedExceptionTypeName))
+    {
+        $actualExceptionTypeName = $Exception.GetType().Name
+        if ($UseFullTypeName)
+        {
+            $actualExceptionTypeName = $Exception.GetType().FullName
+        }
+        # -ieq is case insensitive equals.
+        $exceptionTypeMatched = ($actualExceptionTypeName -ieq $ExpectedExceptionTypeName.Trim())
+        if ($Not -and $exceptionTypeMatched)
+        {
+            $errorMessages += `
+                "Expected an exception of a different type than $ExpectedExceptionTypeName but exception thrown was of that type."
+        }
+        elseif (-not $Not -and -not $exceptionTypeMatched)
+        {
+            $errorMessages += `
+                "Expected $ExpectedExceptionTypeName but exception thrown was $actualExceptionTypeName."
+        }
+
+    }
+
+    if (-not [string]::IsNullOrWhiteSpace($ExpectedExceptionMessage))
+    {
+        # -ilike is case insensitive like.
+        $exceptionMessageMatched = ($Exception.Message -ilike "*$ExpectedExceptionMessage*")
+        if ($Not -and $exceptionMessageMatched)
+        {
+            $errorMessages += `
+                "Expected exception message different than '$ExpectedExceptionMessage' but exception thrown had that message."
+        }
+        elseif (-not $Not -and -not $exceptionMessageMatched)
+        {
+            $errorMessages += `
+                "Expected exception message '$ExpectedExceptionMessage' but actual exception message was '$($Exception.Message)'."
+        }
+    }
+
+    if ($Not)
+    {
+        if ($exceptionTypeMatched -and $exceptionMessageMatched)
+        {
+            return $errorMessages
+        }
+        else
+        {
+            return @()
+        }
+    }
+    else
+    {
+        if ($exceptionTypeMatched -and $exceptionMessageMatched)
+        {
+            return @()
+        }
+        else
+        {
+            return $errorMessages
+        }
+    }
+}
---
+++ PesterTests/ExportedLoggingFunctions.Tests.ps1
@@ -0,0 +1,281 @@
+﻿<#
+.SYNOPSIS
+Tests of the exported logging functions in the Logging module.
+
+.DESCRIPTION
+Pester tests of the logging functions exported from the Logging module.
+#>
+
+# PowerShell allows multiple modules of the same name to be imported from different locations.  
+# This would confuse Pester.  So, to be sure there are not multiple Logging modules imported, 
+# remove all Logging modules and re-import only one.
+Get-Module Logging | Remove-Module -Force
+# Use $PSScriptRoot so this script will always import the Logging module in the Modules folder 
+# adjacent to the folder containing this script, regardless of the location that Pester is 
+# invoked from:
+#                                     {parent folder}
+#                                             |
+#                   -----------------------------------------------------
+#                   |                                                   |
+#     {folder containing this script}                                Modules folder
+#                   \                                                   |
+#                    ------------------> imports                     Logging module folder
+#                                                \                      |
+#                                                 -----------------> Logging.psm1 module script
+Import-Module (Join-Path $PSScriptRoot ..\Modules\Logging\Logging.psm1 -Resolve) -Force
+
+<#
+.SYNOPSIS
+Test function with no arguments that throws an exception.
+#>
+function NoArgsException
+{
+    throw [ArgumentException] "This is the message"
+}
+
+InModuleScope Logging {
+
+    # Need to dot source the helper file within the InModuleScope block to be able to use its 
+    # functions within a test.
+    . (Join-Path $PSScriptRoot .\AssertExceptionThrown.ps1 -Resolve)
+
+    # Gets the MessageFormatInfo hashtable that matches the default configuration.
+    function GetDefaultMessageFormatInfo ()
+    {
+        $messageFormatInfo = @{
+                                RawFormat = '{Timestamp:yyyy-MM-dd hh:mm:ss.fff} | {CallingObjectName} | {MessageType} | {Message}'
+                                WorkingFormat = '$($Timestamp.ToString(''yyyy-MM-dd hh:mm:ss.fff'')) | ${CallingObjectName} | ${MessageType} | ${Message}'
+                                FieldsPresent = @('Message', 'Timestamp', 'CallingObjectName', 'MessageType')
+                            }
+        return $messageFormatInfo
+    }
+
+    function GetCallingDirectoryPath ()
+    {
+        $callStack = Get-PSCallStack
+        $stackFrame = $callStack[0]
+        # Skip this function in the call stack as we've already read it.
+	    $i = 1
+	    while ($stackFrame.ScriptName -ne $Null -and $i -lt $callStack.Count)
+	    {
+		    $stackFrame = $callStack[$i]
+            if ($stackFrame.ScriptName -ne $Null)
+		    {
+                $stackFrameFileName = $stackFrame.ScriptName
+            }
+		    $i++
+	    }
+        $pathOfCallerDirectory = Split-Path -Path $stackFrameFileName -Parent
+        return $pathOfCallerDirectory
+    }
+
+    function GetResetLogFilePath ()
+    {
+        $dateString = Get-Date -Format "_yyyyMMdd"
+        $path = "TestDrive:\Results${dateString}.log"
+        return $path
+    }
+
+    # Sets the Logging configuration to its defaults, apart from LogFileName and LogFilePath.
+    function ResetConfiguration ()
+    {
+        $script:_logConfiguration = Private_DeepCopyHashTable $script:_defaultLogConfiguration
+        $script:_logConfiguration.LogFileName = "TestDrive:\Results.log"
+        $script:_messageFormatInfo = GetDefaultMessageFormatInfo
+        $script:_logFilePath = GetResetLogFilePath
+        $script:_logFileOverwritten = $False
+    }
+
+    Describe 'Write-LogMessage' {             
+
+        BeforeEach {
+            ResetConfiguration
+        }
+
+        It 'throws no error when no Message supplied' {
+            { Write-LogMessage } | Should -Not -Throw
+        }
+
+        It 'writes to log when no Message supplied' {
+            Mock Write-Host
+
+            Write-LogMessage
+
+            Assert-MockCalled -CommandName Write-Host -Scope It -Times 1
+        }
+
+        It 'throws exception if both -IsError and -IsWarning switches set' {
+            { Write-LogMessage -Message 'hello world' -IsError -IsWarning } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsError and -IsInformation switches set' {
+            { Write-LogMessage -Message 'hello world' -IsError -IsInformation } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsError and -IsDebug switches set' {
+            { Write-LogMessage -Message 'hello world' -IsError -IsDebug } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsError and -IsVerbose switches set' {
+            { Write-LogMessage -Message 'hello world' -IsError -IsVerbose } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsError and -IsSuccessResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsError -IsSuccessResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsError and -IsFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsError -IsFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsError and -IsPartialFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsError -IsPartialFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsWarning and -IsInformation switches set' {
+            { Write-LogMessage -Message 'hello world' -IsWarning -IsInformation } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsWarning and -IsDebug switches set' {
+            { Write-LogMessage -Message 'hello world' -IsWarning -IsDebug } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsWarning and -IsVerbose switches set' {
+            { Write-LogMessage -Message 'hello world' -IsWarning -IsVerbose } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsWarning and -IsSuccessResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsWarning -IsSuccessResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsWarning and -IsFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsWarning -IsFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsWarning and -IsPartialFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsWarning -IsPartialFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsInformation and -IsDebug switches set' {
+            { Write-LogMessage -Message 'hello world' -IsInformation -IsDebug } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsInformation and -IsVerbose switches set' {
+            { Write-LogMessage -Message 'hello world' -IsInformation -IsVerbose } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsInformation and -IsSuccessResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsInformation -IsSuccessResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsInformation and -IsFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsInformation -IsFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsInformation and -IsPartialFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsInformation -IsPartialFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsDebug and -IsVerbose switches set' {
+            { Write-LogMessage -Message 'hello world' -IsDebug -IsVerbose } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsDebug and -IsSuccessResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsDebug -IsSuccessResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsDebug and -IsFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsDebug -IsFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsDebug and -IsPartialFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsDebug -IsPartialFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsVerbose and -IsSuccessResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsVerbose -IsSuccessResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsVerbose and -IsFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsVerbose -IsFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsVerbose and -IsPartialFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsVerbose -IsPartialFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsSuccessResult and -IsFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsSuccessResult -IsFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsSuccessResult and -IsPartialFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsSuccessResult -IsPartialFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -IsFailureResult and -IsPartialFailureResult switches set' {
+            { Write-LogMessage -Message 'hello world' -IsFailureResult -IsPartialFailureResult } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Message Type switch parameter may be set'                                        
+        }
+
+        It 'throws exception if both -WriteToHost and -WriteToStreams switches set' {
+            { Write-LogMessage -Message 'hello world' -WriteToHost -WriteToStreams } | 
+                Assert-ExceptionThrown -ExpectedExceptionTypeName ArgumentException `
+                    -ExpectedExceptionMessage 'Only one Destination switch parameter may be set'                                        
+        }
+    }
+}
